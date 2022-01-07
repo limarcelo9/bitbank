c540df6c6f2f5a90422ea266599fd885dbaaa740
unknown <limarcelo90@gmail.com>
2022-01-06 23:01:50 -0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Bem vindo ao ByteBank")
+
+    val titular: String = "Marcelo"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100.0
+
+    println("Titular $titular")
+    println("Numero da conta $numeroConta")
+
+    saldo += 200.0
+    println("Saldo $saldo")
+}
